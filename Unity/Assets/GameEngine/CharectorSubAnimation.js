7e3985538b7946066baf5c6657a14f26075d7dfd
Josh <funinvegas@gmail.com>
2014-03-12 23:06:09 -0700

    Adding Swish animation and updating as state machine

---
+++ Unity/Assets/GameEngine/CharectorSubAnimation.js
@@ -0,0 +1,14 @@
+﻿#pragma strict
+function OnAnimationEvent(event:String) {
+	//Debug.Log("CharectorSubAnimation");
+	var mainChar = transform.parent.gameObject.GetComponent("MainCharector") as MainCharector;
+	//Debug.Log("mainChar = " + mainChar);
+	mainChar.OnAnimationEvent(event);
+}
+function Start () {
+
+}
+
+function Update () {
+
+}
